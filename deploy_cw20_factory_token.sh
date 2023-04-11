6fca0d0c935dde41cd36964d7f7c3b8814363f5f
project-88288 <chaiya.88288@gmail.com>
2023-04-11 18:59:35 +0700

    update

--- deploy_cw20_factory_token.sh
+++ deploy_cw20_factory_token.sh
@@ -1,1 +1,3 @@
 terrain deploy cw20_factory_token --signer test --config-path ./config.terrain_cw20.json
+# when deploy cw20_factory_token first then 
+# get <token_contract_code_id> from refs.terrain.json to determine which <token_contract_code_id> for token_factory
--- deploy_factory_token.sh
+++ deploy_factory_token.sh
@@ -1,1 +1,5 @@
 terrain deploy factory_token --signer test --config-path ./config.terrain.json
+# when deploy factory_token 
+# To determine which <token_contract_code_id>, 
+# check the file refs.terrain.json 
+# from the workspace's root under the cw20_factory_token object.
